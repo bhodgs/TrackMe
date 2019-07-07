4840132483ad2cdfb1f5450947401536ccfa58fa
bhodgs <bhodgs@deakin.edu.au>
2019-07-07 20:23:18 +1000

    ENHANCE: Add html library

---
+++ web/public/app.js
@@ -0,0 +1,24 @@
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+$('#navbar').load('navbar.html')
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+   });
+
+$('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = '/'
+});
+
+$('#send-command').on('click', function() {
+       const command = $('#command').val();
+});
