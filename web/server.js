bf39e135a396a48bb53f50e8d4c81b6435fcbb07
bhodgs <bhodgs@deakin.edu.au>
2019-07-07 20:31:38 +1000

    ENHANCE: Add full web directory

---
+++ web/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const base = `${__dirname}/public`;
+
+app.use(express.static('public'));
+
+
+app.listen(port, () => {
+    console.log(`listening to port: 3000`);
+});
+
+app.get('/', function (req, res) {
+    console.log('Loading device list.');
+    res.sendFile(`${base}/device-list.html`);
+});
+
+app.get('/send-command', (req, res) => {
+    console.log('Loading send command.');
+    res.sendFile(`${base}/send-command.html`);
+});
+
+app.get('/register-device', (req, res) => {
+    console.log('Loading register device.');
+    res.sendFile(`${base}/register-device.html`);
+});
+
+app.get('/about', (req, res) => {
+    console.log('Loading about me.');
+    res.sendFile(`${base}/about-me.html`);
+});
+
+
+app.get('*', (req, res) => {
+    console.log('Loading 404.');
+    res.sendFile(`${base}/404.html`);
+});
+
+
+
+
+
+
+
+
