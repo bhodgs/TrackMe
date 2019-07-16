1f4e84e9f86985b59d80bb771c3ee2713a1c56f3
bhodgs <bhodgs@deakin.edu.au>
2019-07-16 17:52:56 +1000

    NEW: Add user model and user suthentication/registration

--- api/api.js
+++ api/api.js
@@ -1,4 +1,5 @@
 const Device = require('./models/device');
+const User = require('./models/user');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -48,3 +49,63 @@ app.post('/api/devices', (req, res) => {
       : res.send('successfully added device and data');
 }); });
 
+app.post('/api/authenticate', (req, res) => {
+  const {name, password} = req.body;
+  userCheck = User.findOne({"name": name}).then(doc => {
+    if(!doc){ return res.send('User not found.')}
+    else if(doc.password == password) 
+    { 
+      return res.json({
+        success: true,
+        message: 'Authenticated successfully',
+        isAdmin: doc.isAdmin
+     });
+    }
+    else{
+      return res.send('Password incorrect.');
+    }
+  }).catch(err =>{
+    return err
+  });
+});
+
+app.post('/api/register', (req, res) =>{
+  const {name, password, isAdmin} = req.body;
+  User.findOne({"name": name}).then(doc => {
+    if(doc){return res.send('Username taken.')}
+    else{
+      const newUser = new User({
+        name: name,
+        password,
+        isAdmin
+       });
+       newUser.save(err => {
+        return err
+        ? res.send(err)
+        : res.json({
+        success: true,
+        message: 'Created new user'
+        });
+       });
+    }
+  }).catch(err => {
+    return err;
+  })
+})
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    return err
+      ? res.send(err)
+      : res.send(users);
+}); });
+
+app.get('/api/devices/:deviceId/device-history', (req, res) => {
+  const { deviceId } = req.params;
+  Device.findOne({"_id": deviceId }, (err, devices) => {
+  const { sensorData } = devices;
+  return err
+  ? res.send(err)
+  : res.send(sensorData);
+  });
+ });
