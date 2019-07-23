49e4abcfce66263fc5fa1a3e6216a96f7965bbcb
bhodgs <bhodgs@deakin.edu.au>
2019-07-23 14:52:33 +1000

    NEW: Add MQTT APi

---
+++ mqtt/mqtt.js
@@ -0,0 +1,33 @@
+const mqtt = require('mqtt');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const { URL, PORT, USERNAME, PASSWORD } = process.env;
+
+const client = mqtt.connect({
+    host: URL,
+    port: PORT,
+    username: USERNAME,
+    password: PASSWORD
+});
+client.on('connect', () => {
+    console.log('connected');
+});
+app.post('/send-command', (req, res) => {
+    const { deviceId, command }  = req.body;
+    const topic = `/command/${deviceId}`;
+    client.publish(topic, command, () => {
+      res.send('published new message');
+    });
+  });
+const port = 5001;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
